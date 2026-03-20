81e1fe12e0cd8c0431ced213b9a4efe454aed3c5
itintern8 <lari.cavalcante13@gmail.com>
2026-03-20 15:15:32 -0300

    documenting function and adding sound for victory

--- finalscreen.py
+++ finalscreen.py
@@ -6,7 +6,10 @@ SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 FPS = 60
 
+# Função para exibir a tela de vitória
 def victory(screen):
+    pygame.mixer.music.load("assets/sounds/waku-waku.mp3")
+    pygame.mixer.music.play(0)
     try:
         victorybackground = pygame.image.load("assets/paz-mundial.jpeg").convert()
         font_victory = pygame.font.Font("assets/fonts/spy-agency.ttf", 52) 
@@ -17,19 +20,23 @@ def victory(screen):
         print(f"Erro {e}")
         return
 
+    # Textos da tela de vitória
     text_victory = font_victory.render("Você venceu!", True, (250,179,173))
     text_victory2 = font_victory2.render("Anya se tornou uma discípula imperial do Colégio Éden!", True, (255,255,255))
     text_return = font_return.render("Pressione ENTER para jogar novamente", True, (255,255,255))
 
     while True:
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+            if event.type == pygame.QUIT:         
+                pygame.mixer.music.stop()
                 pygame.quit()
                 exit()
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RETURN:
+                    pygame.mixer.music.stop()
                     return "menu"
 
+        # Renderização da tela de vitória
         screen.blit(victorybackground, (0, 0))
         screen.blit(text_victory, (150, 160))
         screen.blit(text_victory2, (50, 240)) 
@@ -37,7 +44,7 @@ def victory(screen):
 
         pygame.display.flip()
 
-
+# Função para exibir a tela de derrota
 def defeat(screen):
     pygame.mixer.music.load("assets/sounds/sorry.mp3")
     pygame.mixer.music.play(0)
@@ -50,7 +57,8 @@ def defeat(screen):
     except FileNotFoundError as e:
         print(f"Erro: {e}")
         return
-
+    
+    # Textos da tela de derrota
     text_defeat = font_defeat.render("Você perdeu!", True, (44,40,39))
     text_defeat2 = font_defeat2.render("Anya recebeu 8 raios e foi expulsa do Colégio Éden!", True, (255,255,255))
     text_return = font_return.render("Pressione ENTER para jogar novamente", True, (255,255,255))
@@ -66,6 +74,7 @@ def defeat(screen):
                     pygame.mixer.music.stop()
                     return "menu"
     
+        # Renderização da tela de derrota
         screen.blit(defeatbackground, (0, 0))
         screen.blit(text_defeat, (160, 160))
         screen.blit(text_defeat2, (75, 240))
